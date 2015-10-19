208bb0518c381f3784400af7cf175a2345b7a53c
青智 <yiming.wym@alipay.com>
2015-10-19 23:01:44 +0800

    增加添加产品的弹出框

--- assets/resources/js/zhilu/productCtrl.js
+++ assets/resources/js/zhilu/productCtrl.js
@@ -1,12 +1,12 @@
 function productCtrl($scope, $http, $location, $document, $window,$modal,$timeout,$stateParams,$state,productInfoService) {
     $scope.queryProduct = queryProduct;
-
+    $scope.showAddProduct = showAddProduct;
     $scope.add = 0;
     var vm = this;
 
-    function queryProduct(productId){
+    function queryProduct(productId) {
 
-        productInfoService.queryProductInfo(productId,function(res){
+        productInfoService.queryProductInfo(productId, function (res) {
             $scope.product = {};
             vm.add = 1;
             $scope.product.id = productId;
@@ -19,8 +19,25 @@ function productCtrl($scope, $http, $location, $document, $window,$modal,$timeou
             console.log('product', $scope.product);
         })
     }
+
+    function showAddProduct() {
+        var modalInstance = $modal.open({
+            templateUrl: 'product/partials/product.add.tpl.html',
+            scope: $scope,
+            controller: 'ModalInstanceCtrl',
+            size: "lg"
+        });
+    }
 }
+
+function ModalInstanceCtrl($scope, $modalInstance) {
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}
+
 
 angular
     .module('zhilu')
-    .controller('productCtrl', productCtrl);
+    .controller('productCtrl', productCtrl)
+    .controller('ModalInstanceCtrl', ModalInstanceCtrl);
